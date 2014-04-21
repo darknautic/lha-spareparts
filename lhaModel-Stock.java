3a37b46e918225518a30220b9de5f96d284e6e6e
darkNautic <blue.darknautic@gmail.com>
2014-04-20 21:50:33 -0500

    Update lhaModel-Stock.java

--- lhaModel-Stock.java
+++ lhaModel-Stock.java
@@ -5,19 +5,19 @@ package com.lhaModel;
  */
 
 
-import com.mongodb.DB;
-import com.mongodb.DBCollection;
-import com.mongodb.DBCursor;
-import com.mongodb.MongoClient;
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mongodb.*;
 import com.mongodb.util.JSON;
+import com.mongodb.util.JSONCallback;
+import org.omg.CORBA.*;
 
 
+import java.lang.Object;
 import java.net.UnknownHostException;
+import java.util.*;
 import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Set;
 
 
 public class Stock {
@@ -59,6 +59,11 @@ public class Stock {
 
     }
 
+
+
+
+
+
     public ArrayList<String> getSpareOptionsArray(){
         DBCollection spares = db.getCollection("spares");
         DBCursor spareOptions = spares.find();
@@ -77,6 +82,127 @@ public class Stock {
 
 
     }
+
+    //---
+
+    public List all(){
+        DBCollection spares = db.getCollection("spares");
+        //List obj = collection.find( query ).skip( 1000 ).limit( 100 ).toArray();
+        List a = spares.find().toArray();
+        return a;
+
+    }
+
+    public void nuevoRegistro(HashMap<String,String> newRecord){
+        DBCollection spares = db.getCollection("spares");
+
+        /*handling system array*/
+        List<String> systemArray = new ArrayList<String>();
+        systemArray = stringFormattedToStringList(newRecord.get("system"));
+        //BasicDBObject system = new BasicDBObject("system",systemArray);
+
+        /*handling models JSON document*/
+        System.out.println("models ==>"+ newRecord.get("model"));
+        //----------------------
+
+        /**
+         *
+         *
+         * {
+         Aveo={
+         standar=[2005]
+         },
+         Ferrari={
+         v6=[2011, 2012, 2013, 2014],
+         v8=[2015],
+         v12=[2014, 2015]
+         },
+         Golf={
+         limited=[2013],
+         standar=[2008, 2009, 2010]
+         }
+         }
+         *
+         */
+            String modelString = new String();
+            modelString = newRecord.get("model");
+            HashMap<String,Object> auxHash = new HashMap<String, Object>();
+            auxHash = formatted01(modelString);
+
+
+
+
+        //----------------------
+
+
+        BasicDBObject newJSON = new BasicDBObject("sparepart",newRecord.get("sparepart")).
+                                append("briefDescription", newRecord.get("briefDescription")).
+                                append("brand", newRecord.get("brand")).
+                                append("partNumber",newRecord.get("partNumber")).
+                                append("system",systemArray).
+                                append("barCode",newRecord.get("barCode")).
+                                append("model", auxHash);
+        //spares.insert(newJSON);
+
+    }
+
+
+    public static List<String> stringFormattedToStringList(String s) {
+
+        List<String> result = new ArrayList<String>();
+        List<String> aux = new ArrayList<String>();
+        //return Arrays.asList(s.substring(1,s.length()-1).replaceAll("\"","").split(","));
+        aux =  Arrays.asList(s.substring(1,s.length()-1).replaceAll("\"","").split(","));
+        // .substring  removes the first an last characters from the string ('[' & ']')
+        // .replaceAll removes all quotation marks from the string (replaces with empty string)
+        // .split brakes the string into a string array on commas (omitting the commas)
+        // Arrays.asList converts the array to a List
+
+        for(int i=0;i<aux.size() ; i++) {
+            result.add(aux.get(i).trim());
+        }
+
+        return result;
+    }
+
+
+
+    public static HashMap<String, Object> formatted01(String modelString){
+        HashMap<String, Object> auxHash = new HashMap<String, Object>();
+        List<String> aux = new ArrayList<String>();
+        aux = Arrays.asList(modelString.substring(1,modelString.length() - 1).split("},"));
+
+        System.out.println("substring -" + modelString.substring(1,modelString.indexOf("=")));
+
+        HashMap<String,String> autos = new HashMap<String, String>();
+        autos.put("a","a");
+        autos.put("b","b");
+        autos.put("c","c");
+
+        String s= new String();
+        for(int i=0; i < aux.size(); i++)
+        {
+            if(i < (aux.size()-1) )
+                s = aux.get(i).trim()+"}";
+            else
+                s = aux.get(i).trim();
+            //auxHash.put(s.substring(0,s.indexOf("=")),s.replace(s.substring(0,s.indexOf("=")+1),""));
+            auxHash.put(s.substring(0,s.indexOf("=")),autos);
+            //**********
+            // ***************************
+            // mandar este string a otra funcion
+            System.out.println(s.replace(s.substring(0,s.indexOf("=")+1),""));
+
+            System.out.println(i + " ==> "+s);
+        }
+
+        System.out.println(auxHash);
+
+        return auxHash;
+
+    }
+
+
 
 
 
